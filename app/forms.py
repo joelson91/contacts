fd60ce134fc56ec92457c774f624e248df5519e1
Joelson <joelsonandradep@gmail.com>
2025-01-18 01:02:11 -0300

    Remove submit button from user form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, TelField, SubmitField
+from wtforms import StringField, EmailField, TelField
 
 
 class UserForm(FlaskForm):
     nome = StringField("Nome")
     telefone = TelField("Telefone")
     email = EmailField("Email")
-    btn_submit = SubmitField("Salvar")
